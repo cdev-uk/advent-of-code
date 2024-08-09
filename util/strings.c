838cb72c431a8c58a83d0588778147718c95697d
Paul Waring <paul@xk7.net>
2024-08-09 15:58:28 +0100

    Make end index exclusive in sub_string utility function
    The end index needs to be exclusive so that we can return an empty
    string if the start and end indexes are the same.

--- util/strings.c
+++ util/strings.c
@@ -14,9 +14,19 @@ void tearDown(void) {
 
 void test_sub_string_empty_string(void) {
     const char *input = "";
-    size_t output_length = 20;
+    size_t output_length = 5;
     char *output = calloc(output_length, sizeof(char));
     size_t sub_string_length = sub_string(input, 0, 0, output, output_length);
+
+    TEST_ASSERT_EQUAL_size_t(0, sub_string_length);
+    TEST_ASSERT_EQUAL_STRING("", output);
+}
+
+void test_sub_string_start_end_equal(void) {
+    const char *input = "abcde";
+    size_t output_length = strlen(input);
+    char *output = calloc(output_length, sizeof(char));
+    size_t sub_string_length = sub_string(input, 2, 2, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(0, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("", output);
@@ -26,7 +36,7 @@ void test_sub_string_first_character(void) {
     const char *input = "abcde";
     size_t output_length = strlen(input);
     char *output = calloc(output_length, sizeof(char));
-    size_t sub_string_length = sub_string(input, 0, 0, output, output_length);
+    size_t sub_string_length = sub_string(input, 0, 1, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(1, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("a", output);
@@ -36,7 +46,7 @@ void test_sub_string_last_character(void) {
     const char *input = "abcde";
     size_t output_length = strlen(input);
     char *output = calloc(output_length, sizeof(char));
-    size_t sub_string_length = sub_string(input, 4, 4, output, output_length);
+    size_t sub_string_length = sub_string(input, 4, 5, output, output_length);
 
     TEST_ASSERT_EQUAL_size_t(1, sub_string_length);
     TEST_ASSERT_EQUAL_STRING("e", output);
